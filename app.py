118dbc9396af4f2e2440248f739fe666fda15d16
xogur <snsk656@naver.com>
2025-04-14 13:06:47 +0900

    이미지

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-# deproject/app/app.py
 import streamlit as st
 import psycopg2
 import pandas as pd
 
+st.set_page_config(layout="wide")
 st.title("🎨 Fashion Trend Color Dashboard")
 
+# PostgreSQL 연결
 conn = psycopg2.connect(
     host="postgres_custom",
     port=5432,
@@ -13,6 +14,17 @@ conn = psycopg2.connect(
     dbname="deproject"
 )
 
+# 데이터 불러오기
 df = pd.read_sql("SELECT * FROM color_trend", conn)
-st.dataframe(df)
 conn.close()
+
+# 시각화
+for idx, row in df.iterrows():
+    col1, col2 = st.columns([1, 5])
+    with col1:
+        st.image(row['image'], width=100)
+    with col2:
+        st.markdown(f"**RGB:** ({row['r']}, {row['g']}, {row['b']})")
+        st.markdown(f"`{row['image']}`")
+
+    st.markdown("---")
